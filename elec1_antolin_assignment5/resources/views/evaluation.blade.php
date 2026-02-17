25277115329f40d512396adaa14f8518395382c2
ChristineAntolin <christineantolin5@gmail.com>
2026-02-17 14:14:07 +0700

    Assignmet 5

--- elec1_antolin_assignment5/resources/views/evaluation.blade.php
+++ elec1_antolin_assignment5/resources/views/evaluation.blade.php
@@ -1,70 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Student Evaluation</title>
+    <title>Student Evaluation System</title>
 </head>
 <body>
 
 <h2>Student Evaluation System</h2>
 
+
+@if(!isset($prelim))
 <form method="POST" action="/evaluation">
     @csrf
-
     <p>Name: <input type="text" name="name" required></p>
     <p>Prelim: <input type="number" name="prelim" required></p>
     <p>Midterm: <input type="number" name="midterm" required></p>
     <p>Final: <input type="number" name="final" required></p>
     <button type="submit">Evaluate</button>
-
 </form>
+@endif
+
 
 @if(isset($prelim))
-
     @php
         $average = ($prelim + $midterm + $final) / 3;
+        if ($average >= 90) $letter = 'A';
+        elseif ($average >= 80) $letter = 'B';
+        elseif ($average >= 70) $letter = 'C';
+        elseif ($average >= 60) $letter = 'D';
+        else $letter = 'F';
+        $remarks = ($average >= 75) ? 'Passed' : 'Failed';
+        if ($average >= 98) $award = 'With Highest Honors';
+        elseif ($average >= 95) $award = 'With High Honors';
+        elseif ($average >= 90) $award = 'With Honors';
+        else $award = 'No Award';
     @endphp
 
-    <h3>Result</h3>
-    <p>Name: {{ $name }}</p>
-    <p>Average: {{ number_format($average,2) }}</p>
-
-    <p>
-        Letter Grade:
-        @if($average >= 90)
-            A
-        @elseif($average >= 80)
-            B
-        @elseif($average >= 70)
-            C
-        @elseif($average >= 60)
-            D
-        @else
-            F
-        @endif
-    </p>
-
-    <p>
-        Remarks:
-        @if($average >= 75)
-            Passed
-        @else
-            Failed
-        @endif
-    </p>
-
-    <p>
-        Award:
-        @if($average >= 98)
-            With Highest Honors
-        @elseif($average >= 95)
-            With High Honors
-        @elseif($average >= 90)
-            With Honors
-        @else
-            No Award
-        @endif
-    </p>
-
+    <div class="result">
+        <h4>Evaluation Result for {{ $name }}</h4>
+        <p><strong>Average:</strong> {{ number_format($average, 2) }}</p>
+        <p><strong>Letter Grade:</strong> {{ $letter }}</p>
+        <p><strong>Remarks:</strong> {{ $remarks }}</p>
+        <p><strong>Award:</strong> {{ $award }}</p>
+    </div>
 @endif
 
 </body>
